197d570c4ee3062a17a3a559d66e30fbaf670080
Mark Bishop <mbishop@duosecurity.com>
2026-04-27 02:03:46 -0400

    Add overlay

--- flake.nix
+++ flake.nix
@@ -48,10 +48,13 @@
                 --set LOCALE_ARCHIVE ${pkgs.glibcLocales}/lib/locale/locale-archive
             '';
           };
-    in {
+    in rec {
       packages = rec {
         tmux = mkTmux fullConf;
         default = tmux;
+      };
+      overlays = final: prev: {
+        tmux = packages.default;
       };
     };
   in
